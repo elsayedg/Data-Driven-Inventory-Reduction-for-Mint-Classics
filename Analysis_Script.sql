7f85570a28d1f938ed03edfadf4dca18b85bebb5
Elsayed Aboelmaaty <eng.sayed.gamal@gmail.com>
2024-02-07 13:44:01 +0200

    last

--- Analysis_Script.sql
+++ Analysis_Script.sql
@@ -8,35 +8,43 @@ this lead us to question do we have over stock
 
 -- what is the moving stocks per year for each warehouse?
 
-SELECT p.warehousecode,
-       Year(o.orderdate) AS year,
+SELECT
+    p.warehousecode,
+       YEAR(o.orderdate) AS YEAR,
        ( ( Sum(od.quantityordered) / Sum(p.quantityinstock) ) * 100 ) AS
        perecent_of_moving_stock
-FROM   products p
-       JOIN orderdetails od
-         ON p.productcode = od.productcode
-       JOIN orders o
-         ON o.ordernumber = od.ordernumber
-GROUP  BY warehousecode,
-          year
-ORDER  BY year,
-          warehousecode; 
+FROM
+    products p
+JOIN orderdetails od
+         ON
+    p.productcode = od.productcode
+JOIN orders o
+         ON
+    o.ordernumber = od.ordernumber
+GROUP BY
+    warehousecode,
+          YEAR
+ORDER BY
+    YEAR,
+          warehousecode;
     
     
 /*
-# warehouseCode	year	perecent_of_moving_stock
-		a		2003	0.6635
-		b		2003	0.5870
-		c		2003	0.6499
-		d		2003	1.0410
-		a		2004	0.6594
-		b		2004	0.6048
-		c		2004	0.6662
-		d		2004	1.0069
-		a		2005	0.7279
-		b		2005	0.6666
-		c		2005	0.7031
-		d		2005	1.0271
+ 
+        warehousecode|YEAR|perecent_of_moving_stock|
+        -------------+----+------------------------+
+        a            |2003|                  0.6635|
+        b            |2003|                  0.5870|
+        c            |2003|                  0.6499|
+        d            |2003|                  1.0410|
+        a            |2004|                  0.6594|
+        b            |2004|                  0.6048|
+        c            |2004|                  0.6662|
+        d            |2004|                  1.0069|
+        a            |2005|                  0.7279|
+        b            |2005|                  0.6666|
+        c            |2005|                  0.7031|
+        d            |2005|                  1.0271|
         
 
 we got the Same picture which is we need to reduce our stock 
@@ -45,7 +53,8 @@ we got the Same picture which is we need to reduce our stock
     
 -- so what if analysis  reducing  the stock by 50 % 
 
-SELECT p.warehousecode,
+SELECT
+    p.warehousecode,
        Sum(o.quantityordered)
        AS total_Orders,
        Sum(p.quantityinstock) - ( Sum(p.quantityinstock) * .5 )
@@ -54,19 +63,24 @@ SELECT p.warehousecode,
                                         Sum(p.quantityinstock) * .5 ) ) ) * 100
        ) AS
        perecent_of_moving_stock
-FROM   products p
-       JOIN orderdetails o
-         ON p.productcode = o.productcode
-GROUP  BY warehousecode
-ORDER  BY perecent_of_moving_stock DESC; 
+FROM
+    products p
+JOIN orderdetails o
+         ON
+    p.productcode = o.productcode
+GROUP BY
+    warehousecode
+ORDER BY
+    perecent_of_moving_stock DESC;
 
 
 /*
-# warehouseCode	total_Orders	total_quantity	perecent_of_moving_stock
-	d	22351		1093435.5		2.0441
-	a	24650		1829776.5		1.3472
-	c	22933		1719785.0		1.3335
-	b	35582		2922016.5		1.2177
+        warehousecode|total_Orders|total_quantity|perecent_of_moving_stock|
+        -------------+------------+--------------+------------------------+
+        d            |       22351|     1093435.5|                  2.0441|
+        a            |       24650|     1829776.5|                  1.3472|
+        c            |       22933|     1719785.0|                  1.3335|
+        b            |       35582|     2922016.5|                  1.2177|
 
 */
 
@@ -74,70 +88,99 @@ ORDER  BY perecent_of_moving_stock DESC;
 -- first let get the numbers in each and what are the capacity ?
 
 WITH groupbywarhouse
-     AS (SELECT warehousecode,
+     AS (
+SELECT
+    warehousecode,
                 productline,
                 Sum(quantityinstock) AS totalQuantity
-         FROM   products
-         GROUP  BY warehousecode,
+FROM
+    products
+GROUP BY
+    warehousecode,
                    productline)
-SELECT *,
+SELECT
+    *,
        Sum(totalquantity)
          OVER(
-           partition BY warehousecode) AS TotalPerWarhouse
-FROM   groupbywarhouse
-ORDER  BY warehousecode,
-          productline; 
+           PARTITION BY warehousecode) AS TotalPerWarhouse
+FROM
+    groupbywarhouse
+ORDER BY
+    warehousecode,
+          productline;
         
         
         
 /*
-	# warehouseCode	productLine		totalQuantity	TotalPerWarhouse
-		a	Motorcycles		69401			131688
-		a	Planes			62287			131688
-		b	Classic Cars		219183			219183
-		c	Vintage Cars		124880			124880
-		d	Ships			26833			79380
-		d	Trains			16696			79380
-		d	Trucks and Buses	35851			79380
-
+        warehousecode|productline     |totalQuantity|TotalPerWarhouse|
+        -------------+----------------+-------------+----------------+
+        a            |Motorcycles     |        69401|          131688|
+        a            |Planes          |        62287|          131688|
+        b            |Classic Cars    |       219183|          219183|
+        c            |Vintage Cars    |       124880|          124880|
+        d            |Ships           |        26833|           79380|
+        d            |Trains          |        16696|           79380|
+        d            |Trucks and Buses|        35851|           79380|
 */
 
 
 -- what are the capacity ?
 
-SELECT *
-FROM   warehouses; 
+SELECT
+    *
+FROM
+    warehouses; 
 /*
-		# warehouseCode	warehouseName	warehousePctCap
-			a	North			72
-			b	East			67
-			c	West			50
-			d	South			75
+        warehouseCode|warehouseName|warehousePctCap|
+        -------------+-------------+---------------+
+        a            |North        |72             |
+        b            |East         |67             |
+        c            |West         |50             |
+        d            |South        |75             |
 
 */
 
+--  What is the maximum number of items that each warehouse can hold?
 
-
+SELECT
+    p.warehouseCode AS code ,
+    w.warehouseName AS Name,
+    w.warehousePctCap AS Capacity, 
+    sum(p.quantityInStock) AS TotalItemInStock,
+    round((sum(p.quantityInStock) /(w.warehousePctCap * .01)), 0) AS TotalCapacity,
+    round((sum(p.quantityInStock) /(w.warehousePctCap * .01)), 0) - sum(p.quantityInStock) AS EmptyFor
+FROM
+    products p
+JOIN warehouses w 
+ON
+    p.warehouseCode = w.warehouseCode
+GROUP BY
+    p.warehouseCode ,
+    w.warehouseName,
+    w.warehousePctCap 
+;
 
 /*
 here is the results  quantityInStock
 
 from this we can calculate the items it takes 
-		Code	Name		percentage    		Capacityin#		emptyFor		  
-		a	North		72		(131,688/.72) = 182,900	        51,212
-		b	East		67		(219,183/.67) = 327,139 	107,956
-		c	West		50		(124,880/.50) = 249,760         124,880
-		d	South		75		( 79,380/.75) = 105,840          26,460
-                
-                
-Taking into account the average delivery time
 
-	    warehouseCode	avg_order_shiped
-		c		  4.6142
-		a		  4.2229
-		b		  4.2101
-		d		  3.8629
+        code|Name |Capacity|TotalItemInStock|TotalCapacity|EmptyFor|
+        ----+-----+--------+----------------+-------------+--------+
+        a   |North|72      |          131688|     182900.0| 51212.0|
+        b   |East |67      |          219183|     327139.0|107956.0|
+        c   |West |50      |          124880|     249760.0|124880.0|
+        d   |South|75      |           79380|     105840.0| 26460.0|
+                        
+Taking into account the average delivery time Question 7 from EDA file 
 
+            warehousecode|avg_order_shiped|
+            -------------+----------------+
+            c            |          4.6142|
+            a            |          4.2229|
+            b            |          4.2101|
+            d            |          3.8629|
+            
 */
 
 -- ===============================
--- EDA_Script.sql
+++ EDA_Script.sql
@@ -4,37 +4,52 @@
 
 -- products table
 
-SELECT *
-FROM   products
-WHERE  productname IS NULL; 
+SELECT
+    *
+FROM
+    products
+WHERE
+    productname IS NULL;
 
 -- orderdetails table 
 
-SELECT *
-FROM   orderdetails
-WHERE  productcode IS NULL; 
+SELECT
+    *
+FROM
+    orderdetails
+WHERE
+    productcode IS NULL;
 
 -- orders table
-
-SELECT *
-FROM   orders
-WHERE  orderdate IS NULL; 
+SELECT
+    *
+FROM
+    orders
+WHERE
+    orderdate IS NULL;
 
 -- productlines table 
 
-SELECT *
-FROM   productlines; 
+SELECT
+    *
+FROM
+    productlines;
 
 -- warehouses table 
 
-SELECT *
-FROM   warehouses; 
+SELECT
+    *
+FROM
+    warehouses;
 
 -- customers table 
 
-SELECT *
-FROM   customers
-WHERE  customername IS NULL; 
+SELECT
+    *
+FROM
+    customers
+WHERE
+    customername IS NULL;
 
 /*
  there is no missing data in our data base  
@@ -47,11 +62,15 @@ WHERE  customername IS NULL;
 
 -- 1- How many products are stored in each warehouse?
 
-SELECT warehousecode,
+SELECT
+    warehousecode,
        Sum(quantityinstock) AS total_quantity
-FROM   products p
-GROUP  BY warehousecode
-ORDER  BY total_quantity DESC; 
+FROM
+    products p
+GROUP BY
+    warehousecode
+ORDER BY
+    total_quantity DESC;
 
 /*
 	warehouseCode|total_quantity|
@@ -64,13 +83,18 @@ ORDER  BY total_quantity DESC;
 
 -- 2- How many items did each warehouse serve?
 
-SELECT warehousecode,
+SELECT
+    warehousecode,
        Sum(quantityordered) AS total_Orders
-FROM   products p
-       JOIN orderdetails o
-         ON p.productcode = o.productcode
-GROUP  BY warehousecode
-ORDER  BY total_orders DESC; 
+FROM
+    products p
+JOIN orderdetails o
+         ON
+    p.productcode = o.productcode
+GROUP BY
+    warehousecode
+ORDER BY
+    total_orders DESC;
 
 /*
 	warehouseCode|total_Orders|
@@ -84,27 +108,31 @@ ORDER  BY total_orders DESC;
 
 -- 3- What is the percentage of moving stock for each warehouse?
 
-SELECT p.warehousecode,
-       Sum(o.quantityordered)                                        AS
+SELECT
+    p.warehousecode,
+       Sum(o.quantityordered) AS
        total_items_Orders,
-       Sum(p.quantityinstock)                                        AS
+       Sum(p.quantityinstock) AS
        total_quantity,
        ( ( Sum(o.quantityordered) / Sum(p.quantityinstock) ) * 100 ) AS
        perecent_of_moving_stock
-FROM   products p
-       JOIN orderdetails o
-         ON p.productcode = o.productcode
-GROUP  BY warehousecode
-ORDER  BY perecent_of_moving_stock DESC; 
+FROM
+    products p
+JOIN orderdetails o
+         ON
+    p.productcode = o.productcode
+GROUP BY
+    warehousecode
+ORDER BY
+    perecent_of_moving_stock DESC;
 
 /*
-	warehouseCode|total_items_Orders|total_quantity|perecent_of_moving_stock|
-	-------------+------------------+--------------+------------------------+
-	d South      |       	   22351|       2186871|                  1.0221|
-	a North      |     	   24650|       3659553|                  0.6736|
-	c west       |     	   22933|       3439570|                  0.6667|
-	b East       |     	   35582|       5844033|                  0.6089|
-	
+        warehousecode|total_items_Orders|total_quantity|perecent_of_moving_stock|
+        -------------+------------------+--------------+------------------------+
+        d            |             22351|       2186871|                  1.0221|
+        a            |             24650|       3659553|                  0.6736|
+        c            |             22933|       3439570|                  0.6667|
+        b            |             35582|       5844033|                  0.6089|
     
     as we can see the South warehouse has the highst percent of moving stock
 
@@ -113,7 +141,8 @@ ORDER  BY perecent_of_moving_stock DESC;
 
 -- 4- How much time does it usually take to deliver an order?
 
-SELECT Datediff(shippeddate, orderdate)
+SELECT
+    Datediff(shippeddate, orderdate)
        AS
        actual_time,
        Datediff(requireddate, orderdate)
@@ -121,21 +150,33 @@ SELECT Datediff(shippeddate, orderdate)
        ( Datediff(requireddate, orderdate) ) - (
        Datediff(shippeddate, orderdate) ) AS
        diff
-FROM   orders o
-WHERE  o.status = 'Shipped'
-ORDER  BY diff; 
+FROM
+    orders o
+WHERE
+    o.status = 'Shipped'
+ORDER BY
+    diff;
 /*
-	# actual_time		expected_time			diff
-		65			9			-56
-		6			6			0
-		6			6			0
-		7			7			0
-		6			6			0
-		6			6			0
-		7			7			0
-		6			6			0
-		5			6			1
-		5			6			1
+        actual_time|expected_time|diff|
+        -----------+-------------+----+
+                 65|            9| -56|
+                  6|            6|   0|
+                  6|            6|   0|
+                  7|            7|   0|
+                  6|            6|   0|
+                  6|            6|   0|
+                  7|            7|   0|
+                  6|            6|   0|
+                  5|            6|   1|
+                  5|            6|   1|
+                  6|            7|   1|
+                  5|            6|   1|
+                  5|            6|   1|
+                  6|            7|   1|
+                  6|            7|   1|
+                  6|            7|   1|
+                  5|            6|   1|
+                  5|            6|   1|
 	etc
 
 		we got an outlier 
@@ -143,39 +184,50 @@ ORDER  BY diff;
 */
 
 -- 5- Outlier investigation
-SELECT o.ordernumber,
+SELECT
+    o.ordernumber,
        o.comments
-FROM   orders o
-       JOIN orderdetails od
-         ON o.ordernumber = od.ordernumber
-WHERE  Datediff(o.requireddate, o.shippeddate) < 0
-GROUP  BY o.ordernumber,
-          o.comments; 
+FROM
+    orders o
+JOIN orderdetails od
+         ON
+    o.ordernumber = od.ordernumber
+WHERE
+    Datediff(o.requireddate, o.shippeddate) < 0
+GROUP BY
+    o.ordernumber,
+          o.comments;
 
 /*
-	# orderNumber		comments
-		10165		This order was on hold because customers's credit limit had been exceeded. 
-				Order will ship when payment is received
+ordernumber|comments                                                                                                           |
+-----------+-------------------------------------------------------------------------------------------------------------------+
+      10165|This order was on hold because customers's credit limit had been exceeded. Order will ship when payment is received|
 	
     it's a payment issue not supplychain issue 
 */
 
 -- 6- How much earlier than usual do we typically deliver orders?
 
-SELECT ( Datediff(requireddate, orderdate) ) - (
+SELECT
+    ( Datediff(requireddate, orderdate) ) - (
        Datediff(shippeddate, orderdate) ) AS
        diff_in_Days,
        Count(*)
-FROM   orders o
-WHERE  o.status = 'Shipped'
-GROUP  BY diff_in_days
-ORDER  BY Count(*) DESC; 
+FROM
+    orders o
+WHERE
+    o.status = 'Shipped'
+GROUP BY
+    diff_in_days
+ORDER BY
+    Count(*) DESC;
 
 /*
-	# diff_in_Days  count(*) 
-		3  	54
-		5  	45
-		4  	42
+        diff_in_Days|Count(*)|
+        ------------+--------+
+                   3|      54|
+                   5|      45|
+                   4|      42|
 		etc 
 	most of the time we deliver early by 3 to 5 days 
 */
@@ -185,38 +237,50 @@ ORDER  BY Count(*) DESC;
 -- this shows how much early each warehouse deliver 
 -- the bigger number is better 
 
-SELECT p.warehousecode,
+SELECT
+    p.warehousecode,
        Avg(Datediff(o.requireddate, o.shippeddate)) AS avg_order_shiped
-FROM   orders o
-       JOIN orderdetails od
-         ON o.ordernumber = od.ordernumber
-       JOIN products p
-         ON od.productcode = p.productcode
-WHERE  o.status = 'Shipped'
-GROUP  BY p.warehousecode
-ORDER  BY avg_order_shiped DESC; 
+FROM
+    orders o
+JOIN orderdetails od
+         ON
+    o.ordernumber = od.ordernumber
+JOIN products p
+         ON
+    od.productcode = p.productcode
+WHERE
+    o.status = 'Shipped'
+GROUP BY
+    p.warehousecode
+ORDER BY
+    avg_order_shiped DESC;
 
 /*
-
-		# warehouseCode		avg_order_shiped
-			c		4.6142
-			a		4.2229
-			b		4.2101
-			d		3.8629
+        warehousecode|avg_order_shiped|
+        -------------+----------------+
+        c            |          4.6142|
+        a            |          4.2229|
+        b            |          4.2101|
+        d            |          3.8629|
 
 */
 
 -- 8- What types of products are stored in each warehouse?
 
-SELECT p.warehousecode,
+SELECT
+    p.warehousecode,
        p.productline,
        Count(o.ordernumber)
-FROM   products p
-       JOIN orderdetails o
-         ON p.productcode = o.productcode
-GROUP  BY p.warehousecode,
+FROM
+    products p
+JOIN orderdetails o
+         ON
+    p.productcode = o.productcode
+GROUP BY
+    p.warehousecode,
           p.productline
-ORDER  BY Count(o.ordernumber); 
+ORDER BY
+    Count(o.ordernumber);
 
 /*
 	warehouseCode|productLine     |count(o.orderNumber)|
@@ -232,15 +296,19 @@ ORDER  BY Count(o.ordernumber);
 
 -- 9- What subcategories do each of the products belong to?
 
-SELECT p.warehousecode,
+SELECT
+    p.warehousecode,
        p.productline,
        Count(p.productcode),
        Sum(p.quantityinstock)
-FROM   products p
-GROUP  BY p.warehousecode,
+FROM
+    products p
+GROUP BY
+    p.warehousecode,
           p.productline
-ORDER  BY Count(p.productcode),
-          Sum(p.quantityinstock); 
+ORDER BY
+    Count(p.productcode),
+          Sum(p.quantityinstock);
 /*
 	warehouseCode|productLine     |count(p.productCode)|sum(p.quantityInStock)|
 	-------------+----------------+--------------------+----------------------+
@@ -255,41 +323,46 @@ ORDER  BY Count(p.productcode),
 */
 
 -- 10- Where does the majority of our customer base reside?
-SELECT DISTINCT country,
+SELECT
+    DISTINCT country,
                 Count(DISTINCT customernumber)
-FROM   customers
-GROUP  BY country
-ORDER  BY Count(*) DESC; 
+FROM
+    customers
+GROUP BY
+    country
+ORDER BY
+    Count(*) DESC;
 
 /*
-	# country	count(distinct customerNumber)
-	USA				36
-	Germany				13
-	France				12
-	Spain				7
-	Australia			5
-	UK				5
-	Italy				4
-	New Zealand			4
-	Canada				3
-	Finland				3
-	Norway				3
-	Singapore			3
-	Switzerland			3
-	Austria				2
-	Belgium				2
-	Denmark				2
-	Ireland				2
-	Japan				2
-	Portugal			2
-	Sweden				2
-	Hong Kong			1
-	Israel				1
-	Netherlands			1
-	Philippines			1
-	Poland				1
-	Russia				1
-	South Africa			1
+        country     |Count(DISTINCT customernumber)|
+        ------------+------------------------------+
+        USA         |                            36|
+        Germany     |                            13|
+        France      |                            12|
+        Spain       |                             7|
+        Australia   |                             5|
+        UK          |                             5|
+        Italy       |                             4|
+        New Zealand |                             4|
+        Canada      |                             3|
+        Finland     |                             3|
+        Norway      |                             3|
+        Singapore   |                             3|
+        Switzerland |                             3|
+        Austria     |                             2|
+        Belgium     |                             2|
+        Denmark     |                             2|
+        Ireland     |                             2|
+        Japan       |                             2|
+        Portugal    |                             2|
+        Sweden      |                             2|
+        Hong Kong   |                             1|
+        Israel      |                             1|
+        Netherlands |                             1|
+        Philippines |                             1|
+        Poland      |                             1|
+        Russia      |                             1|
+        South Africa|                             1|
 
 Most of our customer from USA , Germany and France 
 so more than 50% of our customer out Side USA 
